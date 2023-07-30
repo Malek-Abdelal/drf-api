85140cbff72ae0d35e0df93cba267b2303230884
Malek-Abdelal <malekbassam1144@gmail.com>
2023-07-30 23:31:18 +0300

    Django REST Framework & Docker lab is done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# drf-api
+# Lab 31
+
+# Project Name: Django REST Framework & Docker
+
+# Author: Malek Abdelal
+
+## How to run the project:
+
+Write in the terminal => **python manage.py runserver**
+How to build the Docker image and container => **docker compose up**
---
+++ cars/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Car
+from .serializer import CarSerializer
+# Create your views here.
+
+class CarListView(ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class CarDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
